bb5e0473079a264ffdfbfdd7e49ef9b3f869428f
lidq <David.li@rock-chips.com>
2016-08-13 15:54:35 +0800

    add WNpctool

---
+++ WNpctool/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WNpctool.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_WNPCTOOL_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDR_MAINMENU                    129
+#define IDC_CB_DEVICES                  1000
+#define ID_BTN_WRITE                    1001
+#define IDC_STATIC_SN                   1002
+#define IDC_STATIC_WIFIMAC              1003
+#define IDC_STATIC_LANMAC               1004
+#define IDC_STATIC_BTMAC                1005
+#define IDC_EDIT_SN                     1006
+#define IDC_EDIT_WIFIMAC                1007
+#define IDC_EDIT_LANMAC                 1008
+#define IDC_EDIT_BTMAC                  1009
+#define IDC_LIST2                       1010
+#define IDC_LIST_LOG                    1010
+#define IDC_STATIC_STATUS               1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
